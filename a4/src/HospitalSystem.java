814d3fea1f841355f28bd2e1557faec2d530c25e
drw529 <drw529@mail.usask.ca>
2019-06-03 13:34:27 -0600

    Set .idea file to ignore to prevent IDE conflicts

--- a4/src/HospitalSystem.java
+++ a4/src/HospitalSystem.java
@@ -95,26 +95,12 @@ public class HospitalSystem
         System.out.println("Getting Patient information...");
         System.out.print("Enter the name of the doctor: ");
         String name = consoleIn.nextLine();
-        if (doctors.containsKey(name))
-            throw new RuntimeException("Doctor not added as there already "
-                    + "is a doctor with the name " + name);
 
         System.out.print("Is the doctor a surgeon? (yes or no)");
         String response = consoleIn.nextLine();
-        Doctor d;
-        if (response.charAt(0) == 'y' || response.charAt(0) == 'Y')
-            d = new Surgeon(name);
-        else
-            d = new Doctor(name);
+        boolean isSurgeon = response.charAt(0) == 'y' || response.charAt(0) == 'Y';
 
-        // check to make sure the doctor name doesn't already exsist
-        Doctor sameNumberDoctor = doctors.put(name, d);
-        if (sameNumberDoctor != null)
-        {
-            doctors.put(name, sameNumberDoctor); // put the original doctor back
-            throw new RuntimeException("Name in the doctor dictionary even though "
-                    + "containsKey failed.  Name "  + name + " not entered.");
-        }
+        new NewDoctor(name, isSurgeon);
     }
 
     /**

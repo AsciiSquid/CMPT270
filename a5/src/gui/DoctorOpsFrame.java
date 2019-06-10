e6fc60442d53340c66caef5ec3e2af80fbc86075
drw529 <drw529@mail.usask.ca>
2019-06-10 13:07:05 -0600

    Implemented DoctorOps window TODO: Fix the GUI format

--- a5/src/gui/DoctorOpsFrame.java
+++ a5/src/gui/DoctorOpsFrame.java
@@ -1,4 +1,18 @@
 package gui;
 
-public class DoctorOpsFrame {
+import javax.swing.JFrame;
+
+public class DoctorOpsFrame extends JFrame {
+    /** The standard width for the frame. */
+    public static final int DEFAULT_WIDTH = 350;
+
+    /** The standard height for the frame. */
+    public static final int DEFAULT_HEIGHT = 400;
+
+    public DoctorOpsFrame() {
+        setTitle("Doctor Operations");
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        DoctorOpsPanel panel = new DoctorOpsPanel();
+        add(panel);
+    }
 }
--- a5/src/gui/DoctorOpsPanel.java
+++ a5/src/gui/DoctorOpsPanel.java
@@ -1,4 +1,51 @@
 package gui;
 
-public class DoctorOpsPanel {
+import containers.DoctorMapAccess;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DoctorOpsPanel extends JPanel {
+
+    public DoctorOpsPanel() {
+        setLayout(new BorderLayout());
+
+        JButton addDoctor = new JButton("Add Doctor");
+        addDoctor.setMaximumSize(addDoctor.getPreferredSize());
+        addDoctor.setText("Sample Text");
+        add(addDoctor);
+        addDoctor.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        addDoctor.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                //TODO: Create Add Doctor Window
+            }
+        });
+
+        JButton exitDoctor = new JButton("Exit");
+        addDoctor.setMaximumSize(exitDoctor.getPreferredSize());
+        exitDoctor.setText("Example Text");
+        add(exitDoctor, BorderLayout.CENTER);
+
+        exitDoctor.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                exitDoctor.getTopLevelAncestor().setVisible(false);
+            }
+        });
+
+        JTextField searchDoctor = new JTextField("");
+        searchDoctor.setSize(90, 18);
+        add(searchDoctor, BorderLayout.WEST);
+
+        searchDoctor.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                //TODO: Create Doctor Info Window
+            }
+        });
+//
+//        JOptionPane listDoctor = new JOptionPane(DoctorMapAccess.dictionary());
+//        add(listDoctor, BorderLayout.CENTER);
+    }
 }
